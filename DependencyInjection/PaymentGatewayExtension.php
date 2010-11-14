257a0d26c8e16f380aa3ee296ed605c28c05238e
tirnanog06 <matthewfitz@gmail.com>
2010-11-14 09:23:07 -0500

    DependencyInjection configs

---
+++ DependencyInjection/PaymentGatewayExtension.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Bundle\PaymentGatewayBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class PaymentGatewayExtension extends Extension
+{
+	protected $resources = array(
+		'authorizenet'   => 'authorizenet.xml',
+	);
+
+	public function authorizenetLoad($config, ContainerBuilder $container)
+	{
+		if (!$container->hasDefinition('payment_gateway.authorizenet'))
+		{
+			$loader = new XmlFileLoader($container, __DIR__.'/../Resources/config');
+			$loader->load($this->resources['authorizenet']);
+		}
+		if (isset($config['config']))
+		{
+			$container->setParameter('payment_gateway.authorizenet.config', $config['config']);
+		}
+		return $container;
+	}
+
+	/**
+	 * Returns the recommended alias to use in XML.
+	 *
+	 * This alias is also the mandatory prefix to use when using YAML.
+	 *
+	 * @return string The alias
+	 */
+	public function getAlias()
+	{
+		return 'payment_gateway';
+	}
+
+	/**
+	 * Returns the namespace to be used for this extension (XML namespace).
+	 *
+	 * @return string The XML namespace
+	 */
+	public function getNamespace()
+	{
+		return 'http://www.symfony-project.org/schema/dic/payment_gateway'
+	}
+
+	/**
+	 * Returns the base path for the XSD files.
+	 *
+	 * @return string The XSD base path
+	 */
+	public function getXsdValidationBasePath()
+	{
+		return __DIR__.'/../Resources/config/';
+	}
+}
