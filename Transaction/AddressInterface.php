f73ac32447fe710a7fe7cbb61727a456c8cf5aae
jseverson <j.severson@yahoo.com>
2011-01-01 11:02:00 -0800

    adding email address

--- Transaction/AddressInterface.php
+++ Transaction/AddressInterface.php
@@ -4,9 +4,10 @@ namespace Bundle\PaymentGatewayBundle\Transaction;
 
 interface AddressInterface
 {
-	public function getFirstName();
-	public function getLastName();
-	public function getStreet();
-	public function getState();
-	public function getPostalCode();
+    public function getFirstName();
+    public function getLastName();
+    public function getStreet();
+    public function getState();
+    public function getPostalCode();
+    public function getEmail();
 }
