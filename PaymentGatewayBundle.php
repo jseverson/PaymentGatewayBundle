ff0da5117e26b9f4c78ac3808e0083e163784a3f
ornicar <thibault.duplessis@gmail.com>
2011-02-01 12:28:08 -0800

    Implement missing methods in bundle class

--- PaymentGatewayBundle.php
+++ PaymentGatewayBundle.php
@@ -4,4 +4,15 @@ namespace Bundle\PaymentGatewayBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle as BaseBundle;
 
-class PaymentGatewayBundle extends BaseBundle {}
+class PaymentGatewayBundle extends BaseBundle
+{
+    public function getNamespace()
+    {
+        return __NAMESPACE__;
+    }
+
+    public function getPath()
+    {
+        return strtr(__DIR__, '\\', '/');
+    }
+}
