9497d764c3ef40db9614b6adcbdd4b0a3c778fd2
ornicar <thibault.duplessis@gmail.com>
2011-02-09 23:36:30 -0800

    Update dependency injection extension to make use of FileLocator

--- DependencyInjection/PaymentGatewayExtension.php
+++ DependencyInjection/PaymentGatewayExtension.php
@@ -5,6 +5,7 @@ namespace Bundle\PaymentGatewayBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\FileLocator;
 
 class PaymentGatewayExtension extends Extension
 {
@@ -16,7 +17,7 @@ class PaymentGatewayExtension extends Extension
 	{
 		if (!$container->hasDefinition('payment_gateway.authorizenet'))
 		{
-			$loader = new XmlFileLoader($container, __DIR__.'/../Resources/config');
+			$loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 			$loader->load($this->resources['authorizenet']);
 		}
 		if (isset($config['config']))
