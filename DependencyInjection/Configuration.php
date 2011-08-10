22e60e85b6d8cb56ce2c3427e00030225f384468
ornicar <thibault.duplessis@gmail.com>
2011-08-10 09:17:21 -0700

    Fix configuration and DI classes

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -4,44 +4,28 @@ namespace Bundle\PaymentGatewayBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-class Configuration
+class Configuration implements ConfigurationInterface
 {
     /**
      * Generates the configuration tree.
      *
      * @return \Symfony\Component\DependencyInjection\Configuration\NodeInterface
      */
-    public function getConfigTree()
+    public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('payment_gateway', 'array');
-
-        $this->addGatewaysSection($rootNode);
-
-        return $treeBuilder->buildTree();
-    }
-
-    /**
-     * Adds the configuration for the "clients" key
-     */
-    private function addGatewaysSection(ArrayNodeDefinition $rootNode)
-    {
         $rootNode
             ->children()
-                ->arrayNode('gateways')
-                    ->useAttributeAsKey('id')
-                    ->prototype('array')
-                        ->performNoDeepMerging()
-                        ->children()
-                            ->scalarNode('apiLoginId')->end()
-                            ->scalarNode('transactionKey')->end()
-                            ->scalarNode('postUrl')->end()
-                        ->end()
-                    ->end()
-                ->end()
+                ->scalarNode('apiLoginId')->end()
+                ->scalarNode('transactionKey')->end()
+                ->scalarNode('postUrl')->end()
             ->end()
         ;
+
+        return $treeBuilder;
     }
 }
 
--- DependencyInjection/PaymentGatewayExtension.php
+++ DependencyInjection/PaymentGatewayExtension.php
@@ -2,7 +2,6 @@
 
 namespace Bundle\PaymentGatewayBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -10,50 +9,15 @@ use Symfony\Component\Config\FileLocator;
 
 class PaymentGatewayExtension extends Extension
 {
-    protected $resources = array(
-        'authorizenet'   => 'authorizenet.xml',
-    );
-
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
-        $processor = new Processor();
-        $config = $processor->process($configuration->getConfigTree(), $configs);
+        $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('authorizenet.xml');
 
-        if (empty($config['gateways'])) {
-            throw new InvalidArgumentException('You must define at least one payment gateway');
-        }
-
-        if (empty($config['default_gateway'])) {
-            $keys = array_keys($config['gateways']);
-            $config['default_gateway'] = reset($keys);
-        }
-
-        $clientIdsByName = $this->loadClients($config['clients'], $container);
-
-        $container->setAlias('payment_gateway.client', sprintf('foq_elastica.client.%s', $config['default_client']));
-    }
-
-    /**
-     * Loads the configured clients.
-     *
-     * @param array $config An array of clients configurations
-     * @param ContainerBuilder $container A ContainerBuilder instance
-     */
-    protected function loadClients(array $clients, ContainerBuilder $container)
-    {
-        $clientIds = array();
-        foreach ($clients as $name => $clientConfig) {
-            $clientDef = new Definition('%foq_elastica.client.class%', array($clientConfig));
-            $clientId = sprintf('foq_elastica.client.%s', $name);
-            $container->setDefinition($clientId, $clientDef);
-            $clientIds[$name] = $clientId;
-        }
-
-        return $clientIds;
+        $container->setParameter('payment_gateway.authorizenet.config', $config);
     }
 
     /**
